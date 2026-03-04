9f5698cb4f05bd6857a18e509442bde6b9f51760
Janicefv <taliforero10@gmail.com>
2026-03-04 15:22:37 -0500

    Se guardaron los 2 archivos(index) en html y (codigo) en js

---
+++ codigo.js
@@ -0,0 +1,15 @@
+function encriptar() 
+{
+    let e= 0;
+    let n= 0;
+    let valore="";
+
+    valore= document.getElementById("valor").value;
+    document.getElementById("rta").innerHTML="Respuesta: "+textoe;
+
+
+    
+
+
+
+}
